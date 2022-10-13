6430bfa250b337197366d292eac4d936e8cd39b8
Jeremy Potter <jhpotter@protonmail.com>
2022-10-13 10:48:02 -0700

    Formatting and code style

--- LinkedListExample.java
+++ LinkedListExample.java
@@ -1,4 +1,3 @@
-
 import java.util.NoSuchElementException;
 
 class Node {
@@ -9,11 +8,13 @@ class Node {
         this.next = next;
     }
 }
+
 class LinkedList {
     Node root;
     public LinkedList() {
         this.root = null;
     }
+
     /**
      * Adds the value to the _beginning_ of the list
      * @param value
@@ -22,6 +23,7 @@ class LinkedList {
         // Just add at the beginning
         this.root = new Node(value, this.root);
     }
+
     /**
      * Adds the value to the _end_ of the list
      * @param value
@@ -31,39 +33,47 @@ class LinkedList {
             this.root = new Node(value, null);
             return;
         }
+
         // If it's just one element, add if after that one
         Node n = this.root;
         if(n.next == null) {
             n.next = new Node(value, null);
             return;
         }
+
         // Otherwise, loop until the end and add at the end with a null
         while(n.next != null) {
             n = n.next;
             n.next = new Node(value, null);
         }
     }
+
     /**
      * @return the value of the first element in the list
      */
     public int first() {
         return this.root.value;
     }
+
     /**
      * @return the value of the last element in the list
      */
     public int last() {
         Node n = this.root;
+
         // If no such element, throw an exception
         if(n == null) { throw new NoSuchElementException(); }
+
         // If it's just one element, return its value
         if(n.next == null) { return n.value; }
+
         // Otherwise, search for the end of the list and return the last value
         while(n.next != null) {
             n = n.next;
         }
         return n.value;
     }
+
     /**
      * @return a string representation of the list
      */
@@ -76,6 +86,7 @@ class LinkedList {
         }
         return s;
     }
+
     /**
      * @return the number of elements in the list
      */
--- MethodsTests.java
+++ MethodsTests.java
@@ -2,19 +2,21 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class MethodsTests {
-  @Test 
-  public void testSumEvensLength4() {
-    int[] input1 = { 12, 13, 7, 2};
-    assertEquals(EvensExample.sumEvenIndices(input1), 19);
-  }
-  @Test 
-  public void testSumEvenLength5() {
-    int[] input1 = { 12, 13, 7, 2, 33};
-    assertEquals(EvensExample.sumEvenIndices(input1), 52);
-  }
-  @Test
-  public void testSumEvenLength6() {
-    int[] input1 = { 12, 13, 7, 8, 5, 3};
-    assertEquals(EvensExample.sumEvenIndices(input1), 24);
-  }
+    @Test
+    public void testSumEvensLength4() {
+        int[] input1 = { 12, 13, 7, 2};
+        assertEquals(EvensExample.sumEvenIndices(input1), 19);
+    }
+
+    @Test
+    public void testSumEvenLength5() {
+        int[] input1 = { 12, 13, 7, 2, 33};
+        assertEquals(EvensExample.sumEvenIndices(input1), 52);
+    }
+
+    @Test
+    public void testSumEvenLength6() {
+        int[] input1 = { 12, 13, 7, 8, 5, 3};
+        assertEquals(EvensExample.sumEvenIndices(input1), 24);
+    }
 }
