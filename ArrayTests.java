245d8dc336969a75cb3ae89d60f61e7943c186ea
Jeremy Potter <jhpotter@protonmail.com>
2022-10-13 10:55:03 -0700

    Add failing tests for ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,14 +4,29 @@ import org.junit.*;
 public class ArrayTests {
     @Test
     public void testReverseInPlace() {
-        int[] input1 = { 3 };
-        ArrayExamples.reverseInPlace(input1);
-        assertArrayEquals(new int[]{ 3 }, input1);
+        int[] input = {3};
+        ArrayExamples.reverseInPlace(input);
+        assertArrayEquals(new int[]{3}, input);
     }
 
     @Test
     public void testReversed() {
-        int[] input1 = { };
-        assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+        int[] input = {};
+        int[] output = ArrayExamples.reversed(input);
+        assertArrayEquals(new int[]{}, output);
+    }
+
+    @Test
+    public void testReverseInPlace2() {
+        int[] input = {1, 2, 3};
+        ArrayExamples.reverseInPlace(input);
+        assertArrayEquals(new int[]{3, 2, 1}, input);
+    }
+
+    @Test
+    public void testReversed2() {
+        int[] input = {1, 2, 3};
+        int[] output = ArrayExamples.reversed(input);
+        assertArrayEquals(new int[]{3, 2, 1}, output);
     }
 }
